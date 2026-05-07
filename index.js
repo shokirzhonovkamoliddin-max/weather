198f855626f569a7c464102db69d358c08ff20ca
shokirzhonovkamoliddin-max <shokirzhonovkamoliddin@gmail.com>
2026-05-07 18:39:35 +0500

    finish

--- index.js
+++ index.js
@@ -1,39 +1,21 @@
-const elForm = document.querySelector(".form");
-const elInput = document.querySelector(".input");
-const elCard = document.querySelector(".card");
+const div = document.createElement("div");
+const elbody = document.querySelector("body");
 
-elForm.addEventListener("submit", (e) => {
-    e.preventDefault();
+const api = `https://jsonplaceholder.typicode.com/posts`;
 
-    const inputName = elInput.value.trim();
-    const api = `https://api.openweathermap.org/data/2.5/weather?q=${inputName}&units=metric&appid=c67e3943b1537eb384b2ac2193719538`;
+function showData(user) {
+    elbody.append(div);
 
-    getWeather(api);
-});
+    user.forEach((element) => {
+        const { body, title, id, userId } = element;
 
-const getWeather = (url) => {
-    const request = new XMLHttpRequest();
-
-    request.addEventListener("readystatechange", () => {
-        if (request.status === 200 && request.readyState === 4) {
-            const data = JSON.parse(request.responseText);
-            showWeather(data);
-        }
+        div.innerHTML += `
+            <h2>${title}</h2>
+            <p>id: ${id}</p>
+        `;
     });
-
-    request.open("GET", url);
-    request.send();
-};
-
-function showWeather(weather) {
-    console.log(weather);
-
-    const { name } = weather;
-    const degree = weather.main.temp;
-
-    elCard.innerHTML = `
-        <h2>Shahar : ${name}</h2>
-        <p class="degree">Degree: ${Math.round(degree)} C</p>
-    `;
 }
 
+fetch(api)
+    .then((response) => response.json())
+    .then((data) => showData(data));
